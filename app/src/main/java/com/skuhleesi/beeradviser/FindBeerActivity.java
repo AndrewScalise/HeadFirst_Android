5b721e92309d844ff78ea6f5e4b9a12349e971dd
AndrewScalise <paradigmcomplex@gmail.com>
2017-03-02 09:48:26 -0600

    Add on clicks

--- app/src/main/java/com/skuhleesi/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/skuhleesi/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.skuhleesi.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    //Call when the user clicks the button
+    public void onClickFindBeer(View view){
+        //Get a reference to the text view
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Display the selected item
+        brands.setText(beerType);
     }
 }
